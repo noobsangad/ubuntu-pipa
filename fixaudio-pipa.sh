7d443318ed7a689526d5cf31ef1c5afe3027aa4c
noobsangad <arifbvdi@gmail.com>
2025-06-24 21:34:22 +0700

    Create fixaudio-pipa.sh

---
+++ fixaudio-pipa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "=== Mematikan dan memblokir PipeWire ==="
+systemctl --user mask pipewire-pulse.service
+systemctl --user stop pipewire-pulse.service
+systemctl --user mask pipewire.socket
+systemctl --user stop pipewire.socket
+systemctl --user stop pipewire.service pipewire-pulse.service
+
+echo "=== Mengaktifkan dan memulai PulseAudio ==="
+systemctl --user unmask pulseaudio.service pulseaudio.socket
+systemctl --user enable pulseaudio.service pulseaudio.socket
+systemctl --user start pulseaudio.service pulseaudio.socket
+
+echo "=== Memperbarui paket dan menginstal dependensi ==="
+sudo apt update
+sudo apt install -y ubuntu-restricted-extras gstreamer1.0-pulseaudio
+
+echo "=== Selesai! PulseAudio sekarang aktif. ==="
